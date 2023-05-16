b09e5529ea4b6a43536e938fd5221873cd89c6f7
lucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-05-16 08:59:21 -0300

    create transactions

--- info.md
+++ info.md
@@ -9,3 +9,4 @@
  npx json-server server.json -d 500
  npm i react-hook-form zod
  npm i @hookform/resolvers
+ npm i axios
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -7,6 +7,7 @@ import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 
+
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext)
 
